803f84be2d565feb8bfb75e495c88e9d359277af
olegfomenko2002 <oleg.fomenko2002@gmail.com>
2024-01-17 17:11:38 +0200

    updating grants

--- pkg/auth/grants.go
+++ pkg/auth/grants.go
@@ -5,7 +5,7 @@ import (
 	"github.com/rarimo/rarime-auth-svc/resources"
 )
 
-func GlobalRoleGrant(userDID, orgDID string, role uint32) Grant {
+func UserGlobalRoleGrant(userDID, orgDID string, role uint32) Grant {
 	return func(claim resources.Claim) bool {
 		return claim.Role == role &&
 			claim.Org == orgDID &&
@@ -13,12 +13,26 @@ func GlobalRoleGrant(userDID, orgDID string, role uint32) Grant {
 	}
 }
 
-func GroupRoleGrant(userDID, orgDID string, role uint32, group uuid.UUID) Grant {
+func UserRoleGrant(userDID, orgDID string, role uint32, group uuid.UUID) Grant {
 	return func(claim resources.Claim) bool {
 		return claim.Group != nil && *claim.Group == group &&
 			claim.Org == orgDID &&
 			claim.User == userDID &&
 			claim.Role == role
+	}
+}
+
+func GlobalRoleGrant(orgDID string, role uint32) Grant {
+	return func(claim resources.Claim) bool {
+		return claim.Role == role &&
+			claim.Org == orgDID
+	}
+}
+
+func RoleGrant(orgDID string, role uint32) Grant {
+	return func(claim resources.Claim) bool {
+		return claim.Role == role &&
+			claim.Org == orgDID
 	}
 }
 
